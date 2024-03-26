ebb40f52fdd03c732f52ca52bbee422bbf65c3a3
William Horning <wgyt735yt@gmail.com>
2024-03-26 13:56:02 -0400

    Revert "Matrix bridge"

--- packages/bolt-matrix/events.ts
+++ packages/bolt-matrix/events.ts
@@ -1,10 +1,7 @@
-import { message, Intent, Request, WeakEvent } from './deps.ts';
-import { matrix_plugin } from './mod.ts';
+import { BoltMessage, Intent, Request, WeakEvent } from './deps.ts';
+import MatrixPlugin from './mod.ts';
 
-export async function onEvent(
-	this: matrix_plugin,
-	request: Request<WeakEvent>
-) {
+export async function onEvent(this: MatrixPlugin, request: Request<WeakEvent>) {
 	const event = request.getData();
 	const bot = this.bot.getBot();
 	const intent = this.bot.getIntent();
@@ -19,64 +16,52 @@ export async function onEvent(
 			this.emit('debug', `Failed to join room ${event.room_id}: ${e}`);
 		}
 	}
-	if (event.type === 'm.room.message' && !event.content['m.new_content']) {
-		this.emit(
-			'create_message',
-			await messageToCore(event, intent, this.config.homeserverUrl)
-		);
+	if (event.type === 'm.room.message' && !event['m.new_content']) {
+		this.emit('messageCreate', await messageToCore(event, intent));
 	}
-	if (event.type === 'm.room.message' && event.content['m.new_content']) {
-		this.emit(
-			'edit_message',
-			await messageToCore(event, intent, this.config.homeserverUrl)
-		);
+	if (event.type === 'm.room.message' && event['m.new_content']) {
+		this.emit('messageUpdate', await messageToCore(event, intent));
 	}
 	if (event.type === 'm.room.redaction') {
-		this.emit('delete_message', {
+		this.emit('messageDelete', {
 			id: event.redacts as string,
 			platform: { name: 'bolt-matrix', message: event },
 			channel: event.room_id,
-			timestamp: Temporal.Instant.fromEpochMilliseconds(event.origin_server_ts)
+			timestamp: event.origin_server_ts
 		});
 	}
 }
 
 export async function messageToCore(
 	event: WeakEvent,
-	intent: Intent,
-	homeserverUrl: string
-): Promise<message<WeakEvent>> {
+	intent: Intent
+): Promise<BoltMessage<WeakEvent>> {
 	const sender = await intent.getProfileInfo(event.sender);
 	return {
 		author: {
 			username: sender.displayname || event.sender,
 			rawname: event.sender,
 			id: event.sender,
-			profile: `${sender.avatar_url?.replace(
-				'mxc://',
-				`${homeserverUrl}/_matrix/media/v3/thumbnail/`
-			)}?width=96&height=96&method=scale`
+			profile: sender.avatar_url
 		},
 		channel: event.room_id,
-		id:
-			event.content['m.relates_to']?.rel_type == 'm.replace'
-				? event.content['m.relates_to'].event_id
-				: event.event_id,
-		timestamp: Temporal.Instant.fromEpochMilliseconds(event.origin_server_ts),
-		content: (event.content['m.new_content']?.body ||
-			event.content.body) as string,
-		reply: async (msg: message<unknown>) => {
+		id: event.event_id,
+		timestamp: event.origin_server_ts,
+		content: event.content.body as string,
+		reply: async (msg: BoltMessage<unknown>) => {
 			await intent.sendMessage(event.room_id, coreToMessage(msg));
 		},
 		platform: { name: 'bolt-matrix', message: event }
 	};
 }
 
-export function coreToMessage(msg: message<unknown>) {
+export function coreToMessage(msg: BoltMessage<unknown>) {
 	return {
-		body: msg.content
-			? msg.content
-			: "*this bridge doesn't support anything except text at the moment*",
-		msgtype: 'm.text'
+		content: {
+			body: msg.content
+				? msg.content
+				: "*this bridge doesn't support anything except text at the moment*",
+			msgtype: 'm.text'
+		}
 	};
 }
