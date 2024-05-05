fb5a1e618423925c8b6f0f35d8e5012ba8139529
Jersey <wgyt735yt@gmail.com>
2024-05-04 22:59:49 -0400

    try fixing compat stuff

--- packages/lightning/src/cli/mod.ts
+++ packages/lightning/src/cli/mod.ts
@@ -8,13 +8,17 @@ const cli_args = parseArgs(args(), {
 
 const cmd = cli_args._[0];
 
+if (!("Deno" in globalThis)) {
+	const { Deno } = await import("npm:@deno/shim-deno@0.19.1")
+	// @ts-ignore this works
+	globalThis.Deno = Deno
+}
+
 if (cmd === 'version') {
 	console.log('0.6.0');
 } else if (cmd === 'run') {
-	await setup_shim()
 	run(cli_args.config);
 } else if (cmd === 'migrations') {
-	await setup_shim()
 	migrations();
 } else {
 	console.log('lightning v0.6.0 - cross-platform bot connecting communities');
@@ -26,11 +30,4 @@ if (cmd === 'version') {
 	console.log('version: shows version');
 	console.log('Options:');
 	console.log('--config <string>: absolute path to config file');
-}
-
-async function setup_shim() {
-	if ("Deno" in globalThis) return;
-	const Deno = await import("npm:@deno/shim-deno@0.19.1")
-	// @ts-ignore this works
-	globalThis.Deno = Deno.Deno
 }
