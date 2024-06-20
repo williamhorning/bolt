c460b05e9fb0fdda5200366dbed7cbad8f44fd9f
Jersey <wgyt735yt@gmail.com>
2024-06-19 20:29:27 -0400

    add version check

--- packages/lightning/src/lightning.ts
+++ packages/lightning/src/lightning.ts
@@ -45,7 +45,7 @@ export class lightning extends EventEmitter<plugin_events> {
 		setup_bridges(this);
 
 		for (const p of this.config.plugins || []) {
-			if (!p.support.includes('0.7.0')) continue;
+			if (!p.support.includes('0.7.0') || !p.support.includes('0.7.1')) continue;
 			const plugin = new p.type(this, p.config);
 			this.plugins.set(plugin.name, plugin);
 			(async () => {
