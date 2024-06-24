903e4ae614b14db156456cea71d2e4925770d5e9
Jersey <wgyt735yt@gmail.com>
2024-06-23 23:12:06 -0400

    multiple message support

--- packages/lightning/src/lightning.ts
+++ packages/lightning/src/lightning.ts
@@ -1,8 +1,8 @@
-import { EventEmitter, RedisClient } from '../deps.ts';
-import { setup_bridges } from './bridges/setup_bridges.ts';
-import { default_cmds } from './cmds.ts';
-import { type command, setup_commands } from './commands.ts';
-import type { create_plugin, plugin, plugin_events } from './plugins.ts';
+import { EventEmitter, RedisClient } from "../deps.ts";
+import { setup_bridges } from "./bridges/setup_bridges.ts";
+import { default_cmds } from "./cmds.ts";
+import { type command, setup_commands } from "./commands.ts";
+import type { create_plugin, plugin, plugin_events } from "./plugins.ts";
 
 /** configuration options for lightning */
 export interface config {
@@ -45,14 +45,15 @@ export class lightning extends EventEmitter<plugin_events> {
 		setup_bridges(this);
 
 		for (const p of this.config.plugins || []) {
-			if (!p.support.includes('0.7.0')) continue;
-			const plugin = new p.type(this, p.config);
-			this.plugins.set(plugin.name, plugin);
-			(async () => {
-				for await (const event of plugin) {
-					this.emit(event.name, ...event.value);
-				}
-			})();
+			if (p.support.some((v) => ["0.7.0", "0.7.1"].includes(v))) {
+				const plugin = new p.type(this, p.config);
+				this.plugins.set(plugin.name, plugin);
+				(async () => {
+					for await (const event of plugin) {
+						this.emit(event.name, ...event.value);
+					}
+				})();
+			}
 		}
 	}
 }
--- packages/lightning/src/plugins.ts
+++ packages/lightning/src/plugins.ts
@@ -6,7 +6,7 @@ import type { deleted_message, message } from './messages.ts';
 
 /** the way to make a plugin */
 export interface create_plugin<
-	plugin_type extends plugin<plugin_type['config']>,
+	plugin_type extends plugin<plugin_type['config'], string | string[]>,
 > {
 	/** the actual constructor of the plugin */
 	type: new (l: lightning, config: plugin_type['config']) => plugin_type;
@@ -31,7 +31,7 @@ export type plugin_events = {
 };
 
 /** a plugin for lightning */
-export abstract class plugin<cfg> extends EventEmitter<plugin_events> {
+export abstract class plugin<cfg, idtype extends string | string[] = string> extends EventEmitter<plugin_events> {
 	/** access the instance of lightning you're connected to */
 	lightning: lightning;
 	/** access the config passed to you by lightning */
@@ -40,11 +40,11 @@ export abstract class plugin<cfg> extends EventEmitter<plugin_events> {
 	abstract name: string;
 
 	/** create a new plugin instance */
-	static new<T extends plugin<unknown>>(
+	static new<T extends plugin<T["config"]>>(
 		this: new (l: lightning, config: T['config']) => T,
 		config: T['config'],
 	): create_plugin<T> {
-		return { type: this, config, support: ['0.7.0'] };
+		return { type: this, config, support: ['0.7.1'] };
 	}
 
 	constructor(l: lightning, config: cfg) {
@@ -54,29 +54,29 @@ export abstract class plugin<cfg> extends EventEmitter<plugin_events> {
 	}
 
 	/** this should return the data you need to send to the channel given */
-	abstract create_bridge(channel: string): Promise<unknown>;
+	abstract create_bridge(channel: string): Promise<unknown> | unknown;
 
 	/** this is used to bridge a NEW message */
 	abstract create_message(
 		message: message,
 		channel: bridge_channel,
-		edit_id?: string,
+		edit_id?: idtype,
 		reply_id?: string,
-	): Promise<string>;
+	): Promise<idtype>;
 
 	/** this is used to bridge an EDITED message */
 	abstract edit_message(
 		message: message,
 		channel: bridge_channel,
-		edit_id: string,
+		edit_id: idtype,
 		reply_id?: string,
-	): Promise<string>;
+	): Promise<idtype>;
 
 	/** this is used to bridge a DELETED message */
 	abstract delete_message(
 		message: deleted_message,
 		channel: bridge_channel,
-		delete_id: string,
+		delete_id: idtype,
 		reply_id?: string,
-	): Promise<string>;
+	): Promise<idtype>;
 }
