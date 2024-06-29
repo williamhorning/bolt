fe5c8f66a0653e143ab8fa4bd3e363acff78d18d
Jersey <wgyt735yt@gmail.com>
2024-06-29 19:23:07 -0400

    add support for a lot more, edits and deletion still needs work

--- packages/bolt-telegram/deps.ts
+++ packages/bolt-telegram/deps.ts
@@ -1,9 +1,10 @@
 export {
-	lightning,
-	plugin,
 	type bridge_channel,
+	type deleted_message,
+	lightning,
 	type message,
-	type deleted_message
+	plugin,
 } from '../lightning/mod.ts';
-export { Bot, Context } from 'https://deno.land/x/grammy@v1.21.1/mod.ts';
-export { parseArgs } from 'https://deno.land/std@0.215.0/cli/parse_args.ts';
+export { Bot, Context, type CommandContext } from 'https://deno.land/x/grammy@v1.21.1/mod.ts';
+export type { Message } from 'https://deno.land/x/grammy@v1.25.1/types.ts';
+export { Hono } from 'jsr:@hono/hono@4.5.0-rc.2';
--- packages/bolt-telegram/mod.ts
+++ packages/bolt-telegram/mod.ts
@@ -4,84 +4,74 @@ import {
 	type deleted_message,
 	type lightning,
 	type message,
-	parseArgs,
 	plugin,
 } from './deps.ts';
-import { tgtocore } from './messages.ts';
+import { from_telegram } from './telegram_message.ts';
+import { from_lightning } from './lightning_message.ts';
+import { setup_proxy } from './telegram_file_proxy.ts';
 
 export type telegram_config = {
-	token: string;
+	/** the token for the bot */
+	bot_token: string;
+	/** the port the plugins proxy will run on */
+	plugin_port: number;
+	/** the publically accessible url of the plugin */
+	plugin_url: string;
 };
 
 export class telegram_plugin extends plugin<telegram_config, string[]> {
 	name = 'bolt-telegram';
 	bot: Bot;
 
-	constructor(l: lightning, config: telegram_config) {
-		super(l, config);
-		this.bot = new Bot(config.token);
-		this.register_cmds();
+	constructor(l: lightning, cfg: telegram_config) {
+		super(l, cfg);
+		this.bot = new Bot(cfg.bot_token);
 		this.bot.on('message', async (ctx) => {
-			const msg = await tgtocore(ctx, config);
+			const msg = await from_telegram(ctx, cfg);
 			if (!msg) return;
 			this.emit('create_message', msg);
 		});
 		this.bot.on('edited_message', async (ctx) => {
-			const msg = await tgtocore(ctx, config);
+			const msg = await from_telegram(ctx, cfg);
 			if (!msg) return;
 			this.emit('edit_message', msg);
 		});
 		// turns out it's impossible to deal with messages being deleted due to tdlib/telegram-bot-api#286
+		setup_proxy(cfg);
 		this.bot.start();
 	}
 
-	private register_cmds() {
-		this.bot.command(
-			'start',
-			(ctx) =>
-				ctx.reply(
-					"Hey there! Bolt's up and running. Run /help for more info.",
-				),
-		);
-		const cmds = [...this.lightning.commands.keys()];
-		this.bot.command(cmds, (ctx) => {
-			const cmd = ctx
-				.entities()
-				.find((e) => e.type === 'bot_command')
-				?.text?.replace('/', '') || 'help';
-			const rest_of_message = ctx.msg.text.replace(`/${cmd}`, '').trim();
-			const args = parseArgs(rest_of_message.split(' '));
-			this.emit('run_command', {
-				channel: ctx.msg.chat.id.toString(),
-				cmd,
-				subcmd: (args._[0] as string) || undefined,
-				opts: args,
-				plugin: 'bolt-telegram',
-				reply: async (msg) => {
-					// TODO(jersey): find better way to transform content
-					await ctx.reply(msg.content || 'no content');
-				},
-				timestamp: Temporal.Instant.fromEpochSeconds(ctx.msg.date),
-			});
-		});
-	}
-
-	// deno-lint-ignore require-await
-	async create_bridge(channel: string) {
+	create_bridge(channel: string) {
 		return channel;
 	}
 
-	is_bridged() {
-		return false;
-	}
+	async create_message(
+		message: message,
+		bridge: bridge_channel,
+		_: undefined,
+		reply_id?: string,
+	) {
+		const content = from_lightning(message);
+		const messages = [];
 
-	async create_message(message: message, bridge: bridge_channel) {
-		// TODO(jersey): find better way to transform content
-		const result = await this.bot.api.sendMessage(
-			bridge.id,
-			message.content || 'nocontent',
-		);
-		return [result.message_id.toString()];
+		for (const msg of content) {
+			const result = await this.bot.api[msg.function](
+				bridge.id,
+				msg.value,
+				{
+					reply_parameters: reply_id
+						? {
+							message_id: Number(reply_id),
+						}
+						: undefined,
+					parse_mode: 'MarkdownV2',
+				},
+			);
+
+			messages.push(result.message_id.toString());
+		}
+
+		return messages;
 	}
 
 	async edit_message(
@@ -89,11 +79,17 @@ export class telegram_plugin extends plugin<telegram_config, string[]> {
 		bridge: bridge_channel,
 		edit: string[],
 	) {
-		// TODO(jersey): find better way to transform content
+		const content = from_lightning(message)[0];
+
+		console.log(Number(edit[0]));
+
 		await this.bot.api.editMessageText(
 			bridge.id,
 			Number(edit[0]),
-			message.content || 'nocontent',
+			content.value,
+			{
+				parse_mode: 'MarkdownV2',
+			},
 		);
 		return edit;
 	}
@@ -101,9 +97,12 @@ export class telegram_plugin extends plugin<telegram_config, string[]> {
 	async delete_message(
 		_: deleted_message,
 		bridge: bridge_channel,
-		message_id: string[],
+		messages: string[],
 	) {
-		await this.bot.api.deleteMessage(bridge.id, Number(message_id[0]));
-		return message_id;
+		for (const id of messages) {
+			await this.bot.api.deleteMessage(bridge.id, Number(id));
+		}
+
+		return messages;
 	}
 }
