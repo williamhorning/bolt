392cd6d9e348fbd2c889aaf116bd33d258be364e
William Horning <github@williamhorning.dev>
2024-07-05 14:13:45 -0400

    implement telegram support (#54)
    adds support for a relay bridge for telegram by adding the bolt-telegram plugin
    
    Signed-off-by: Jersey <wgyt735yt@gmail.com>

---
+++ packages/bolt-telegram/deps.ts
@@ -0,0 +1,10 @@
+export {
+	type bridge_channel,
+	type deleted_message,
+	lightning,
+	type message,
+	plugin,
+} from '../lightning/mod.ts';
+export { Bot, Context, type CommandContext } from 'https://deno.land/x/grammy@v1.21.1/mod.ts';
+export type { Message } from 'https://deno.land/x/grammy@v1.25.1/types.ts';
+export { Hono } from 'jsr:@hono/hono@4.5.0-rc.2';
